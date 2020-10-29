1efb5971d86c0a460cf9a4d6bc09ff594394b906
Paulo Carvalho <pauloej.carvalho@gmail.com>
2020-10-29 19:44:46 -0300

    feat(front): adding handleAddRepository and handleRemoveRepository

--- src/App.js
+++ src/App.js
@@ -14,26 +14,36 @@ function App() {
   }, []);
 
   async function handleAddRepository() {
-    // TODO
+    const response = await api.post('repositories', {
+      "title": `React New Project - ${Date.now()}`,
+      "url": "http://github.com/paulo-carvalho93",
+	    "techs": ["Node.js", "React.js"]
+    });
+
+    const repository = response.data;
+    setRepositories([...repositories, repository]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    await api.delete(`repositories/${id}`);
+
+    setRepositories(repositories.filter(
+      repository => repository.id !== id
+    ))
   }
 
   return (
     <div>
-      <ul data-testid="repository-list">
-        {repositories.map(repository => 
-          <li>
-            {repository.title}
-            <button onClick={() => handleRemoveRepository(1)}>
-              Remover
-            </button>
-          </li>
-        )}
-      </ul>
-
+        <ul data-testid="repository-list">
+          {repositories.map(repository =>
+            <li key={repository.id}>
+              {repository.title}
+              <button onClick={() => handleRemoveRepository(repository.id)}>
+                Remover
+              </button>
+            </li>
+          )}    
+        </ul>
       <button onClick={handleAddRepository}>Adicionar</button>
     </div>
   );
